d947a0515b374c723b57e233111f7b3cad1c9537
Braduloff <55711892+Braduloff@users.noreply.github.com>
2019-09-29 21:50:45 +0300

    added files SamuraiJS

---
+++ precache-manifest.97d747b75fd12d388aefce92db0fd20a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0459fdabcd7007926c98774a5b1d0b",
+    "url": "/demo-samurai/index.html"
+  },
+  {
+    "revision": "18f2b90b87d0e7147665",
+    "url": "/demo-samurai/static/css/3.ee388f0e.chunk.css"
+  },
+  {
+    "revision": "bcaefbdfe12557776f89",
+    "url": "/demo-samurai/static/css/4.56fe1bf8.chunk.css"
+  },
+  {
+    "revision": "6669c10da07e74e8e366",
+    "url": "/demo-samurai/static/css/main.08a1339a.chunk.css"
+  },
+  {
+    "revision": "8d4bb32d3c4531727216",
+    "url": "/demo-samurai/static/js/2.0bf01fa4.chunk.js"
+  },
+  {
+    "revision": "18f2b90b87d0e7147665",
+    "url": "/demo-samurai/static/js/3.274b61fc.chunk.js"
+  },
+  {
+    "revision": "bcaefbdfe12557776f89",
+    "url": "/demo-samurai/static/js/4.813ff2a9.chunk.js"
+  },
+  {
+    "revision": "6669c10da07e74e8e366",
+    "url": "/demo-samurai/static/js/main.e6c5636b.chunk.js"
+  },
+  {
+    "revision": "1d779d870d20708dad9c",
+    "url": "/demo-samurai/static/js/runtime~main.ef89019d.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/demo-samurai/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/demo-samurai/static/media/user.8b1dbb1f.png"
+  }
+]);
